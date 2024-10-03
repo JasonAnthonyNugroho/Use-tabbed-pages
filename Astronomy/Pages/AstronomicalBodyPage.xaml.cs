d4c4daea0009d960db8923d1a87fb31407d08184
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-10-03 08:09:44 +0700

    <<Navigate to the details page>>
    Now we need to perform the navigation. In AstronomicalBodiesPage.xaml.cs, create click-event handlers for each Button on the page.

--- Astronomy/Pages/AstronomicalBodyPage.xaml.cs
+++ Astronomy/Pages/AstronomicalBodyPage.xaml.cs
@@ -3,9 +3,14 @@ namespace Astronomy.Pages;
 public partial class AstronomicalBodyPage : ContentPage
 {
 
-    public AstronomicalBodyPage()
+    public AstronomicalBodiesPage()
     {
         InitializeComponent();
+
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
     }
 
     void UpdateAstroBodyUI(string astroName)
