fc3001aaa9373e8dbd8a91eacd8ff2c287c41383
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-10-03 08:35:05 +0700

    <BUG> To send data to the AstronomicalBodyPage, add a query parameter string to the routing information. The string is of the form ?astroName=astroBodyToDisplay.
    To receive the data on the AstronomicalBodyPage, first create a class-level property to hold the incoming data. Name it AstroName.
    
    Here, the UpdateAstroBodyUI(astroName) is a helper function used to update the user interface immediately when the AstroName property is set.
    
    Then you need to decorate the class with an annotation that maps the incoming query parameter to the property you created.

--- Astronomy/Pages/AstronomicalBodyPage.xaml.cs
+++ Astronomy/Pages/AstronomicalBodyPage.xaml.cs
@@ -1,17 +1,27 @@
 namespace Astronomy.Pages;
 
+[QueryProperty(nameof(AstroName), "astroName")]
 public partial class AstronomicalBodyPage : ContentPage
 {
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
 
-    public AstronomicalBodiesPage()
+            // this is a custom function to update the UI immediately
+            UpdateAstroBodyUI(astroName);
+        }
+    }
+    public AstronomicalBodyPage()
     {
         InitializeComponent();
-
         btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
         btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
         btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
         btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
-
     }
 
     void UpdateAstroBodyUI(string astroName)
